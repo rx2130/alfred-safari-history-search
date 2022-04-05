05efba9176eb478d34bfc591f4edba610c4589f0
Ray Xue <xuerx@amazon.com>
2022-04-05 11:21:54 -0700

    Remove alfred-workflow dependency

--- README.md
+++ README.md
@@ -1,25 +1,20 @@
-Alfred Safari History Search
-=================
+# Alfred Safari History Search
 
 Search Safari browse history from Alfred. Support Sierra, High Sierra adn Mojave. (Earlier os untested)
 
-Demo
-----
+## Demo
 
 ![][demo]
 
-Note for MacOS 10.14 Mojave users
----------------------------------
+## Note for MacOS 10.14 Mojave users
+
 If you got the `unable to open database file` error, go to `System Preferences > Security & Privacy > Privacy > Full Disk Access` and give `Alfred 3.app` permission to access Safari data :)
 
-Download
---------
+## Download
 
 Get Alfred Safari History Search from [GitHub][gh-releases].
 
-
-Usage
------
+## Usage
 
 - `hsi` — Show list of recent 20 Safari browse history
 - `hsi <query>` — Search for history matching `<query>`
@@ -28,17 +23,10 @@ Usage
   - `⌘+L` — Show full title, URL, browse date in Alfred's Large Type window
   - `⌘+C` - Copy link URL
 
+## Licensing
 
-Licensing, thanks etc.
-----------------------
-
-[MIT][mit].
-
-It's heavily based on [Alfred-Workflow][alfred-workflow], also [MIT-licensed][mit].
-
-
+[MIT][mit]
 
 [mit]: http://opensource.org/licenses/MIT
-[alfred-workflow]: http://www.deanishe.net/alfred-workflow/
 [gh-releases]: https://github.com/rx2130/alfred-safari-history-search/releases
 [demo]: https://raw.githubusercontent.com/rx2130/alfred-safari-history-search/master/demo.gif
--- src/searchSafariHistory.py
+++ src/searchSafariHistory.py
@@ -1,49 +1,30 @@
-#!/usr/bin/python
-# encoding: utf-8
-
 import sys
-from workflow import Workflow3, ICON_WEB, ICON_WARNING
-
+import json
 import sqlite3
 import os
 from datetime import datetime
 
-__author__ = 'rx2130'
-__version__ = '1.0'
 
-UPDATE_SETTINGS = {
-    'github_slug': 'rx2130/alfred-safari-history-search',
-    'version': __version__,
-    'frequency': 7
-}
-HELP_URL = 'https://github.com/rx2130/alfred-safari-history-search'
-ICON_UPDATE = os.path.join(os.path.dirname(__file__), 'update-available.png')
-PATH = os.path.expanduser('~/Library/Safari/History.db')
-HISTORY_LIMIT = 20
+PATH = os.path.expanduser("~/Library/Safari/History.db")
+HISTORY_LIMIT = 40
 
 
-def searchSafariHistory(args):
-    sql, query = searchPatternCmd(args)
+def searchSafariHistory():
+    query = ["%{}%".format(w) for w in sys.argv[1].split(" ") if w]
 
-    with sqlite3.connect(PATH) as con:
-        for r in con.execute(sql, query):
-            yield r
-
-
-def searchPatternCmd(args):
-    query = [u'%{}%'.format(w) for w in args.split(' ') if w]
-
-    sql = '''
+    sql = f"""
     SELECT title, url, visit_time + 978307200, title || ' ' || url AS s
     FROM history_visits AS v
     INNER JOIN history_items AS i
     ON v.history_item = i.id
-    WHERE title <> '' {}
+    WHERE title <> ''
+    {"AND s LIKE ? " * len(query)}
     ORDER BY visit_time DESC
-    LIMIT {}
-    '''.format('AND s LIKE ? ' * len(query), HISTORY_LIMIT)
+    LIMIT {HISTORY_LIMIT}
+    """
 
-    return sql, query
+    with sqlite3.connect(PATH) as con:
+        return con.execute(sql, query)
 
 
 def timeBeautify(time):
@@ -51,40 +32,30 @@ def timeBeautify(time):
     time = datetime.fromtimestamp(time)
     now = datetime.now()
     if time.date() == now.date():
-        res = time.strftime('%-I:%M %p')
+        res = time.strftime("%-I:%M %p")
     else:
-        res = time.strftime('%A,%B %-d,%Y')
-    return '[{}] '.format(res)
+        res = time.strftime("%A,%B %-d,%Y")
+    return "[{}] ".format(res)
 
 
-def main(wf):
-    # workflow update
-    if wf.update_available:
-        wf.add_item(
-            'A newer version is available',
-            '↩ to install update',
-            autocomplete='workflow:update',
-            icon=ICON_UPDATE)
-
-    # search history
-    r = None
-    for r in searchSafariHistory(wf.args[0]):
+def main():
+    items = []
+    for r in searchSafariHistory():
         title, url, time, _ = r
         time = timeBeautify(time)
-        wf.add_item(
-            title,
-            time + url,
-            arg=url,
-            valid=True,
-            icon=ICON_WEB,
-            copytext=url,
-            largetext=u'{}\n{}\n{}'.format(title, url, time))
-    if r is None:
-        wf.add_item('No history found', icon=ICON_WARNING)
+        items.append(
+            {
+                "title": title,
+                "subtitle": time + url,
+                "arg": url,
+                "text": {
+                    "copy": url,
+                    "largetype": "{}\n{}\n{}".format(title, url, time),
+                },
+            }
+        )
+    print(json.dumps({"items": items}))
 
-    wf.send_feedback()
 
-
-if __name__ == '__main__':
-    wf = Workflow3(help_url=HELP_URL, update_settings=UPDATE_SETTINGS)
-    sys.exit(wf.run(main))
+if __name__ == "__main__":
+    main()
